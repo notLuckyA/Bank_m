11647443bc2b91391758e57709512a9c45671b0b
Aleks <aleks.1606sh@gmail.com>
2020-11-04 15:17:28 +0300

    Second act

--- app/src/main/java/com/example/bank/MainActivity.java
+++ app/src/main/java/com/example/bank/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.bank;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView kurs;
     Button otd;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         kurs = (TextView) findViewById(R.id.kurs);
@@ -26,9 +34,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Date currentDate = new Date();
         DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
         String dateText = dateFormat.format(currentDate);
-        kurs.setText("Курс валют\n" + "                                                 " + dateText + "\n\nUSD:79,10                                EUR:92,74");
+        kurs.setText("Курс валют на:\n" + "                                                 " + dateText + "\n\nUSD:79,10                                EUR:92,74");
         otd.setOnClickListener(this);
     }
+
 
     @Override
     public void onClick(View v) {
--- app/src/main/java/com/example/bank/Otdel.java
+++ app/src/main/java/com/example/bank/Otdel.java
@@ -1,17 +1,53 @@
 package com.example.bank;
 
+
+import java.time.LocalTime;
+
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Otdel extends AppCompatActivity  {
+public class Otdel extends AppCompatActivity {
+    TextView rabota1;
+    TextView rabota2;
+    TextView time1;
+    TextView time2;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otdel);
+        rabota1 = (TextView) findViewById(R.id.rabota1);
+        rabota2 = (TextView) findViewById(R.id.rabota2);
+        time1 = (TextView) findViewById(R.id.time1);
+        time2 = (TextView) findViewById(R.id.time2);
+        LocalTime n1 = LocalTime.of(00, 00);
+        LocalTime n2 = LocalTime.of(8, 00);
+        LocalTime k1 = LocalTime.of(00, 00);
+        LocalTime k2 = LocalTime.of(18, 00);
+        LocalTime now = LocalTime.now();
+        time1.setText("Часы работы  " + n1 + "-" + k1);
+        time2.setText("Часы работы  " + n2 + "-" + k2);
+        boolean t1 = now.isBefore(k1);
+        if (t1 = true) {
+            rabota1.setTextColor(Color.GREEN);
+            rabota1.setText("Работает");
+        } else {
+            rabota1.setTextColor(Color.RED);
+            rabota1.setText("Не работает");
+        }
+        boolean t2 = now.isBefore(k2);
+        if (t1 = true) {
+            rabota2.setTextColor(Color.GREEN);
+            rabota2.setText("Работает");
+        } else {
+            rabota2.setTextColor(Color.RED);
+            rabota2.setText("Не работает");
+        }
     }
 }
